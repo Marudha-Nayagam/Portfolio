f2227ecdca0eec8effef82bbf67c0fe39a6e174d
Marudha-Nayagam <marudhanayagam98@gmail.com>
2023-10-03 18:26:01 +0530

    profile picture added

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    // You can add logic here to handle form submission, such as sending an email or storing data in a database.
-    // For now, let's just log the form data to the console.
-    const formData = new FormData(event.target);
-    formData.forEach((value, key) => {
-        console.log(`${key}: ${value}`);
-    });
-
-    // Optionally, you can display a confirmation message to the user.
-    alert('Message sent! Thank you.');
-});
-
 
 document.addEventListener('DOMContentLoaded', function () {
     // Project Modals
